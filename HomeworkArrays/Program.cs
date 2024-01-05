7130d80630d66261ef84d557d5864ec6ce4df597
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-05 07:11:55 -0600

    Initiazlied Commit

--- HomeworkArrays/Program.cs
+++ HomeworkArrays/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+/*
+ * Create an array of 3 names. Ask the user which
+ * number to select. When the user gives you a number, return that name.
+ * Make sure to check for invalid numbers.
+ */
+
